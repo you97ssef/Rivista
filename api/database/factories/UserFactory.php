6dfd17b6b1c331c7f352b79691f8ae4d9b778671
Youssef BAHI <46852751+you97ssef@users.noreply.github.com>
2022-07-15 22:58:44 +0200

    done comment factory and youssef user

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -25,7 +26,7 @@ class UserFactory extends Factory
             'role' => fake()->randomElement([UserRole::ADMIN, UserRole::USER]),
             'email' => fake()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'), // password
             'remember_token' => Str::random(10),
         ];
     }
