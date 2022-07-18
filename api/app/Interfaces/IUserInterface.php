aa8fcdbb2c8a9c3343d420eaf08eb1aa27eecd4e
Youssef BAHI <you97ssef@gmail.com>
2022-07-18 23:18:12 +0200

    added more functions to user repo

--- api/app/Interfaces/IUserInterface.php
+++ api/app/Interfaces/IUserInterface.php
@@ -9,7 +9,12 @@ interface IUserRepo
     public function all();
     public function get(int $id): ?User;
     public function getWithEmail(String $email): ?User;
-    
+    public function getWithSlug(String $slug): ?User;
+
+    public function rivistas($id);
+    public function likes($id);
+    public function comments($id);
+
     public function save(User $user, array $data): bool;
     
     public function delete(User $user): Bool;
