36ed82fa139aa50da0c0a684c37bc0d0d951da5c
Youssef BAHI <you97ssef@gmail.com>
2022-07-18 22:20:10 +0200

    added category data and fake data

--- api/database/factories/CategoryFactory.php
+++ api/database/factories/CategoryFactory.php
@@ -17,7 +17,10 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word,
+            'slug' => $this->faker->slug,
+            'description' => $this->faker->text,
+            'image' => $this->faker->imageUrl(),
         ];
     }
 }
--- api/database/migrations/2022_07_18_195939_create_categories_table.php
+++ api/database/migrations/2022_07_18_195939_create_categories_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
