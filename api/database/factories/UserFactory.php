f20dd7d8c6a8f0af8b084f7b2a42ea8814ab1e6c
Youssef BAHI <you97ssef@gmail.com>
2022-07-18 22:20:24 +0200

    fixed user factory

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -18,7 +19,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'slug' => Str::slug(fake()->name()),
+            'role' => fake()->randomElement([UserRole::ADMIN, UserRole::USER]),
             'email' => fake()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
