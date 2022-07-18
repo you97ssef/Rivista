e5cbdc26be3d549458728c89caf8514818941049
Youssef BAHI <you97ssef@gmail.com>
2022-07-18 22:37:04 +0200

    added rivistas table and data

---
+++ api/database/factories/RivistaFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rivista>
+ */
+class RivistaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'slug' => $this->faker->slug,
+            'text' => $this->faker->text,
+            'image' => $this->faker->imageUrl(),
+            'views' => $this->faker->randomNumber(),
+            'user_id' => $this->faker->numberBetween(1, 10),
+            'category_id' => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Category::factory(10)->create();
+        \App\Models\Rivista::factory(1000)->create();
     }
 }
