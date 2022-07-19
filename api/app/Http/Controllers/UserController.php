1d18449ded96e996126aa67ac1dfdd3f4054f0d9
Youssef BAHI <you97ssef@gmail.com>
2022-07-19 21:22:56 +0200

    done user controller

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\UserRole;
+use App\Http\Response;
 use App\Interfaces\IUserRepo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -11,5 +15,58 @@ class UserController extends Controller
     public function __construct(IUserRepo $userRepo)
     {
         $this->userRepo = $userRepo;
+    }
+
+    public function update(Request $request)
+    {
+        $validatedData = $request->validate([
+            'first_name' => 'nullable|string',
+            'last_name' => 'nullable|string',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = $request->user();
+
+        if(!Hash::check($validatedData['password'], $user->password)) return Response::BadRequest('Invalid password');
+
+        if ($this->userRepo->save($user, $validatedData))
+            return Response::NoContent();
+
+        return Response::BadRequest('Could not update this user.');
+    }
+
+    public function changeRole(Request $request)
+    {
+        $validatedData = $request->validate([
+            'role' => "required|string|in:${UserRole::ADMIN},${UserRole::USER}",
+            'user_id' => 'required|integer',
+        ]);
+
+        if(!$user = $this->userRepo->get($validatedData['user_id'])) return Response::BadRequest('User not found');
+
+        if ($this->userRepo->save($user, $validatedData))
+            return Response::NoContent();
+
+        return Response::BadRequest('Could not update this user.');
+    }
+
+    public function delete(Request $request)
+    {
+        $validatedData = $request->validate([
+            'password' => 'required|integer',
+        ]);
+
+        $user = $request->user();
+
+        if(!Hash::check($validatedData['password'], $user->password)) return Response::BadRequest('Invalid password');
+        
+        foreach ($user->tokens as $token) {
+            $token->delete();
+        }
+
+        if ($this->userRepo->delete($user))
+            return Response::NoContent();
+
+        return Response::BadRequest('Could not delete this user.');
     }
 }
