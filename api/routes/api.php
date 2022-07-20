4cade7206e3df7c07de1bb8456d3e2f261e05e39
Youssef BAHI <46852751+you97ssef@users.noreply.github.com>
2022-07-20 13:28:25 +0200

    added auth routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +13,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// AUTH ROUTES
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::put('/reset-password', [AuthController::class, 'resetPassword']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware('auth:sanctum')->group(function () {
+    // AUTH ROUTES   
+    Route::delete('/logout', [AuthController::class, 'logout']);
+    Route::post('/resend-email-verification', [AuthController::class, 'resendEmailVerification']);
 });
