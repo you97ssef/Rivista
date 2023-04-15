6012836d098e5c48ec70fbaec2a31bf8f6376fab
Youssef BAHI <you97ssef@gmail.com>
2023-04-15 22:35:00 +0200

    done, registered and tested media repo

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -9,10 +9,12 @@ use App\Interfaces\ICommentRepo;
 use App\Data\CommentRepo;
 use App\Interfaces\ILikeRepo;
 use App\Data\LikeRepo;
+use App\Data\MediaRepo;
 use App\Interfaces\IRivistaRepo;
 use App\Data\RivistaRepo;
 use App\Interfaces\IUserRepo;
 use App\Data\UserRepo;
+use App\Interfaces\IMediaRepo;
 use App\Interfaces\IMediaService;
 use App\Services\MediaService;
 
@@ -31,6 +33,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(ILikeRepo::class, LikeRepo::class);
         $this->app->bind(IRivistaRepo::class, RivistaRepo::class);
         $this->app->bind(IUserRepo::class, UserRepo::class);
+        $this->app->bind(IMediaRepo::class, MediaRepo::class);
 
         $this->app->bind(IMediaService::class, MediaService::class);
     }
--- api/tests/Feature/MediaServiceTest.php
+++ api/tests/Feature/MediaServiceTest.php
@@ -2,19 +2,23 @@
 
 namespace Tests\Feature;
 
+use App\Interfaces\IMediaRepo;
 use App\Interfaces\IMediaService;
+use App\Models\Media;
 use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class MediaServiceTest extends TestCase
 {
     private readonly IMediaService $service;
+    private readonly IMediaRepo $repo;
 
     public function setUp(): void
     {
         parent::setUp();
 
         $this->service = app(IMediaService::class);
+        $this->repo = app(IMediaRepo::class);
     }
 
     public function test_service()
@@ -25,5 +29,19 @@ class MediaServiceTest extends TestCase
         $this->assertTrue(isset($data['delete_hash']));
 
         $this->assertTrue($this->service->remove($data['delete_hash']));
+    }
+
+    public function test_repo()
+    {
+        $data = [
+            'link' => 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+            'delete_hash' => 'dQw4w9WgXcQ',
+            'type' => 'video',
+        ];
+
+        $this->assertTrue($this->repo->save($media = new Media(), $data));
+        $this->assertTrue(isset($media->id));
+
+        $this->assertTrue($this->repo->delete($media));
     }
 }
